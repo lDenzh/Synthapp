1182991f14c3878798e13ca9daf8fe96d2622d0d
OscarNord <73446581+OscarNord@users.noreply.github.com>
2023-01-30 15:59:06 +0100

    Update server.py

--- sanic-backend/server.py
+++ sanic-backend/server.py
@@ -5,7 +5,7 @@ app = Sanic("SynthApp")
 
 @app.route("/data")
 async def test(request):
-    return json({"message": "Hello from the backend!"})
+    return json({"message": "Hello from the backend-boi!"})
 
 if name == "main":
     app.run(host="0.0.0.0", port=8000)
