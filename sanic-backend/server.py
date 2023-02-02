6fb4cbd4a170e84f55d7075bbb8f7ab3cd4d0600
lDenzh <robmart99@gmail.com>
2023-02-02 21:29:54 +0100

    axios update, server testing

--- sanic-backend/server.py
+++ sanic-backend/server.py
@@ -1,11 +1,16 @@
 from sanic import Sanic
 from sanic.response import json
+from sanic import text
 
 app = Sanic("SynthApp")
 
-@app.route("/data")
-async def test(request):
-    return json({"message": "Hello from the backend-boi!"})
+@app.get('/')
+async def hello(request):
+    return text("OK!")
+
+#@app.route("/data")
+#async def test(request):
+#    return json({"message": "Hello from the backend-boi!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
