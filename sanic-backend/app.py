76c65bd92859289a815560c99a807b6800c85d5a
Robin Martinsen <72741881+lDenzh@users.noreply.github.com>
2023-02-06 12:37:32 +0100

    CORS

--- sanic-backend/app.py
+++ sanic-backend/app.py
@@ -1,6 +1,8 @@
 from sanic import Sanic
 from sanic.response import json
 from sanic import text
+from cors import add_cors_headers
+from options import setup_options
 
 app = Sanic("SynthApp")
 
@@ -12,5 +14,14 @@ async def hello(request):
 async def test(request):
     return json({"message": "Hello from the backend-boi!"})
 
+    
+# Add OPTIONS handlers to any route that is missing it
+app.register_listener(setup_options, "before_server_start")
+
+# Fill in CORS headers
+app.register_middleware(add_cors_headers, "response")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
+
+    
