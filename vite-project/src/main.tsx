67a7119864683f2a80f6d9f303a22bb75fb3df6d
Robin Martinsen <72741881+lDenzh@users.noreply.github.com>
2023-02-27 17:59:39 +0100

    Ny component for file upload

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import DragDropFiles from './App1'
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <DragDropFiles />
   </React.StrictMode>,
 )
