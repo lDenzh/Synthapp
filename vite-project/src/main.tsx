0ccf7eb450dc2a8567042fe4332a93305c0de781
Robin Martinsen <72741881+lDenzh@users.noreply.github.com>
2023-03-06 13:36:55 +0100

    changes to app1, removed gt comp and integrated

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -8,12 +8,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <div style ={{
-      display: "grid",
-      gridTemplateColumns: "1fr 1fr"
-    }}>
       <DragDropFiles />
-      <GTinput />
-    </div>
   </React.StrictMode>,
 )
